ef03586972d72d5e5205bd165ea254575b80b1c9
FlpGamess <pereirafelipe2003@hotmail.com>
2025-07-08 20:41:00 -0300

    Delete src directory

--- src/GerenciadorFaixa.jsx
+++
@@ -1,5 +0,0 @@
-const GerenciadorFaixa = ({ faixa, referencia }) => {
-  return <audio src={faixa} ref={referencia} />;
-};
-
-export default GerenciadorFaixa;
--- src/assets/capitulos/livro.js
+++
@@ -1,6 +0,0 @@
-import faixa1 from "./f1.mp3";
-import faixa2 from "./Wide Awake.mp3";
-
-const livro = [faixa1, faixa2];
-
-export default livro;
